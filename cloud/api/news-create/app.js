46c1daf93e8dfacb81543fb53584680bb731c5af
Tarsis de Lima <br.tmvdl@gmail.com>
2022-06-07 03:33:32 -0300

    Fixing '/news/create' endpoint

--- cloud/api/news-create/app.js
+++ cloud/api/news-create/app.js
@@ -1,9 +1,21 @@
-const newsIndex = require('/jornal/commons/db').in('news')
+const { NotFoundError } = require('/jornal/commons/errors')
 
-module.exports = ({ body: { title, image, text } }, res) => {
+const md = require('/jornal/commons/middlewares')
+const db = require('/jornal/commons/db')
+const newsIndex = db.in('news')
+const usersIndex = db.in('users')
+
+module.exports = ({ headers: { token }, body: { title, description, category, image, text } }, res) => {
+  const user = md.loginUserByToken(token)
+  const user_id = user.getId()
+
+  if (!user) {
+    throw new NotFoundError('User not found.', { user_id })
+  }
+
   const news = newsIndex.new()
   const created_at = Date.now().toString()
 
-  news.writeMany({ title, image, text, created_at })
+  news.writeMany({ title, description, category, image, text, user_id, created_at })
   return res.json({ id: news.getId(), created_at })
 }
