70560766bd2d4a697b377fbe0bf8834aef502349
Tarsis de Lima <br.tmvdl@gmail.com>
2022-06-08 16:32:18 -0300

    Moving common files for APIs

--- cloud/commons/parser.js
+++
@@ -1,7 +0,0 @@
-const renderFile = (template = '', params = {}) =>
-  Object.keys(params)
-    .reduce((html, param) => html.replace(`##${param}##`, params[param]), template)
-
-module.exports = {
-  renderFile,
-}
